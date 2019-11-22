8a75e649faa27a45bbfed21b6d5a47e53c19bdcc
Davit Martiryan <davitmartiryan1995@gmail.com>
2019-11-22 17:50:19 +0400

    changed directory  from CLAS12c to J-Psi

--- compile_TestReadFile.sh
+++ compile_TestReadFile.sh
@@ -1,1 +1,1 @@
-g++ TestReadFile.cc -o TestReadFile.exe `root-config --cflags --libs` -L'/media/best/TOSHIBA EXT/CLAS12c/hipo/lib' -lhipo4 -llz4 -I'/media/best/TOSHIBA EXT/CLAS12c/hipo/hipo4' -fPIC
+g++ TestReadFile.cc -o TestReadFile.exe `root-config --cflags --libs` -L'/media/best/TOSHIBA EXT/J-Psi/hipo/lib' -lhipo4 -llz4 -I'/media/best/TOSHIBA EXT/J-Psi/hipo/hipo4' -fPIC
